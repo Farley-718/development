46b5d7da1b6e99f83d57df380cd7c20e816ffdd8
Farley Digital <farleydigital@farleys-air.mynetworksettings.com>
2024-02-15 18:19:31 -0500

    working on perimeter script

---
+++ class 1/variable_operator.py
@@ -0,0 +1,46 @@
+#variables
+ 
+# greeting = "Hello World"
+# print(greeting)
+
+# Addition
+#print(5+4)
+#print(4+7)
+#print(1+3)
+
+# Subtraction
+#print(1-1)
+#print(3-2)
+#print(4-2)
+# Multiplication
+#print(3*3)
+#print(4*8)
+#print(6*400)
+
+# Division
+#print(64/12)
+#print(50/2)
+#print(24/3)
+# Exponents
+#print(2**5)
+#print(4**3)
+#print(64**3)
+# Integer Division
+# print(10//4)
+# print(34//5)
+# print(55//7)
+# Modulo/Mod/Remainder
+# print(34%5)
+# print(6%2)
+# print(45%4)
+# Program to find the perimeter of a rectangle
+#P=2(l+w)
+length = 50
+width = 10
+perimeter = 2 * ( length + width )
+print(perimeter)
+
+# Data Types
+
+
+# Integer
