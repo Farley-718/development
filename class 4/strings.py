c9e73933d95c74da676666031eea36ec9ce6303e
Farley Digital <farleydigital@farleys-air.mynetworksettings.com>
2024-02-28 20:04:20 -0500

    class 5

---
+++ class 4/strings.py
@@ -0,0 +1,15 @@
+first_name = 'farley'
+last_name = 'toussaint'
+full_name = first_name + ''+ last_name
+print(full_name)
+x= 'hello'
+str= 'HELLO'.lower()
+print(x)
+
+test_character = 'b'
+test_string = 'bananas'
+print(test_character in test_string)
+
+animal = 'lion' 
+length_of_animal= len(animal)
+print(length_of_animal)
