b1acc0040934c916f91482f409248d08dad63586
Farley Digital <farleydigital@farleys-air.mynetworksettings.com>
2024-03-20 19:35:24 -0400

    update

---
+++ class 12/class_12_listpt2_.py
@@ -0,0 +1,62 @@
+# List comprehension
+vegetables = ['broccoli', 'kale', 'onion', 'garlic', 'kale']
+
+
+''' Exercise
+You are given a list of integers. Write a Python program to create a new list that only includes the even numbers from the original list.
+You can do this in one line with a list comprehension.
+Example:
+original_list = [34, 57, 81, 92, 2, 13]
+new_list = [34, 92, 2]
+'''
+
+''' List comprehension for conditions'''
+# With a For Loop
+original_list = [34, 57, 81, 92, 2, 13]
+new_list = []
+
+
+
+# With list comprehension
+original_list = [34, 57, 81, 92, 2, 13]
+
+
+
+''' List comprehension with expressions'''
+
+# Lets add 5 to every item in this list
+numbers = [0, 2, 3, 8, 9, 11, 20]
+
+
+# We can add two lists
+first_list = [1, 2, 3]
+second_list = [4, 5, 6]
+
+
+
+# Multiply items from two lists
+list1 = [1, 2, 3, 4, 5]
+list2 = [10, 20, 30, 40, 50]
+
+
+
+'''
+Exercise
+
+You work for a sales company and must generate a list of all customers who get a certain discount. The criteria for getting a discount is that they're over 60 years old and have made at least 5 purchases.
+You have a list of customers over 60, and a list of customers who have made at least 5 purchases. Use a list comprehension to output a list of customers that fit both criteria for the discount. You can do this in one line of code.
+Example:
+over_60_years = ['Dominic', 'Linda', 'Simone', 'Swathi', 'Olaf']
+over_5_purchases = ['Finn', 'Simone', 'Aaron', 'Dominic']
+Output: ['Dominic', 'Simone']
+
+'''
+
+over_60_years = ['Dominic', 'Linda', 'Simone', 'Swathi', 'Olaf']
+over_5_purchases = ['Finn', 'Simone', 'Aaron', 'Dominic']
+
+customer_discount = []
+
+# For Loop
+
+# List comprehension
---
+++ project_1/snippet.py
@@ -0,0 +1,56 @@
+import re
+
+
+''' What are some of the things we may come across while building this project'''
+
+'''Initialization and prompt (and testing)'''
+
+first_input, second_input = '', ''
+
+while True:
+    first_input = input("Please enter your data")
+    second_input =  input("Please enter your data")
+
+    print(first_input, second_input)
+    break
+
+
+
+
+
+'''Handling error messages with a list (and testing)'''
+
+error_messages = ['Error message 1', 'Error Message 2', 'This is my new message']
+
+
+# if 5 > 7:
+#     print(error_messages[0])
+# else:
+#     print(error_messages[2])
+
+
+'''Testing a string'''
+
+# Example of string testing (testing for lowercase letter)
+
+# test_string = 'Popeye1989'
+
+# Testing for uppercase as first letter
+# first_letter = test_string[0]
+# lower_case_test = first_letter.islower()
+
+# if lower_case_test:
+#     print("This string failed lowercase testing")
+# else:
+#     print("This string passed lowercase testing")
+
+
+
+''' Taken usernames '''
+sample_word = 'black'
+sample_list = ['green', 'blue', 'orange', 'yellow', 'purple']
+
+# if sample_word in sample_list:
+#     print("Word exists in the list")
+# else:
+#     print("Word does not exist in list")
