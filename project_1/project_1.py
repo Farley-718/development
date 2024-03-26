85e30080cfb60806c987d7b3a9abcc250a287be5
Farley Digital <farleydigital@farleys-air.mynetworksettings.com>
2024-03-25 22:07:02 -0400

    update

--- project_1/project_1.py
+++ project_1/project_1.py
@@ -1,18 +1,3 @@
-'''username & password setup PJ 1'''
-import hashlib # for hashing passwords 
-# database to store user data (for practice)
 
-users_db = {}
-def register(username, password):
-    if username in users_db:
-        print("Username already exists. Please choose a different username.")
-        return False
 
-    hash_password = hashlib.sha256(password.encode()).hexdigest()
-    print("Account created successfully. You can now log in")
-    return True
-def login(username, password):
-    if username not in users_db:
-        print("User not found. Please register first .")
-        return False
 
