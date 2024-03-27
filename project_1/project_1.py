2a4c3b95107f75d8453b8745d92a7a92810dd95e
Farley Digital <farleydigital@farleys-air.mynetworksettings.com>
2024-03-26 20:36:35 -0400

    update

--- project_1/project_1.py
+++ project_1/project_1.py
@@ -1,3 +1,30 @@
+def login(users):
+    while True :
+        username = input("please enter a username: ")
+        password = input("please enter password: ")
+        for u in users :
+            if username == u [0] and password == u[1]:
+                return username
+            print("username or password is incorrect. please try again !")
 
 
 
+
+
+
+  
+
+
+    
+
+
+
+
+
+
+
+
+
+users = [['johnjay','cba321'],['farley','smart123'],['quan','543smart']]
+username = login(users)
+print(username,"Has logged in")
