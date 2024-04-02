c6a8add20ae2a44086d7a9aba3813453e7faf49b
Farley Digital <farleydigital@Farleys-MacBook-Air.local>
2024-04-01 22:36:04 -0400

    update

--- assignments/assignment2.py
+++ assignments/assignment2.py
@@ -11,3 +11,32 @@ To do this, use boolean statements on your string.
 Test your code on a few inputs to make sure it works!
 
 '''
+# Taking input from the user and sanitize it 
+email = input("Enter an email address: ").strip()
+
+# Check if the email address is valid
+# is_valid = "." in email[-3:] and "@" in email[:-4] and " " not in email
+
+# Results
+# print(is_valid)
+
+# testing
+test_emails = ["test@mail.com","no@spaces.com","yes@mail.com"
+               "farley@mail.com"]
+
+# print("\nAdditional testing:")
+# for test_email in test_emails:
+#     is_valid = "." in test_email[-3:] and "@" in test_email[:-4] and " " not in test_email
+#     print(f"{test_email}: {is_valid}")
+# if len(email) >= 7 and "." in email[-2:] and "@" in email[:-3] and " " not in email:
+#     print(True)
+# else:
+#     print(False)
+
+# Check if the email address is valid
+is_valid = len(email) >= 7 and "." in email[-3:] and "@" in email[:-4] and " " not in email
+
+# Print the result
+print(is_valid)
+
+# Is the result now correct? (Yes/No)
