d04ee94715be6f99e1fc8cded08e52f7c573a641
Farley Digital <farleydigital@farleys-air.mynetworksettings.com>
2024-04-08 18:07:23 -0400

    update

---
+++ tuples/tuples.py
@@ -0,0 +1,40 @@
+''' Tuples & Sets '''
+
+'''
+Fun Facts about Tuples
+    -ordered and unchangeable (example, storing a username and password, storing an RGB value that must not change)
+    -can't add or remove items
+    -round brackets
+    -faster than lists
+    -used to store constants
+    -used heterogeneous data structures (integers, floats, strings, etc) for example someone's age, gender and last name, (15, 'M', 'JONES')
+    -heterogenous data structures save memory. why? lists need to over-allocate to account for new elements
+    -readability
+    -lets the programmer or other programmer know, this data collection should not be altered
+'''
+# Count & Indexing
+
+my_number_tuple = (1, 2, 2, 3, 3, 4, 5, 6, 7, 8, 8, 8, 8, 9, 10, 10, 10, 10)
+
+# Use the count Tuple method to count how many instances we have for 2, 3, 8, 9, 10
+
+
+
+my_letter_tuple = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i')
+# Use the index tuple method to return the position of letters b, d, f, h, and i
+
+
+# Unpacking
+user = ('Joe', 'Smith', 24)
+
+
+
+# Loop through list of Tuples
+
+weekdays = [("Monday", 1), ("Tuesday", 2), ("Wednesday", 3), ("Thursday", 4), ("Friday", 5), ("Saturday", 6), ("Sunday", 7)]
+
+
+# Adding tuples
+first_tuple = (1, 2, 3)
+second_tuple = (4, 5, 6)
+
