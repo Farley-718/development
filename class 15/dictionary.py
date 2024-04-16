f1376b0621418a53031d3b2550e3721e7222d254
Farley Digital <farleydigital@farleys-air.mynetworksettings.com>
2024-04-15 20:38:15 -0400

    update

--- class 15/dictionary.py
+++ class 15/dictionary.py
@@ -10,10 +10,16 @@ from statistics import mode
 
 # How do we create a dictionary?
 
+user_data = {"user id": 400, "name": "Fritz"}
+# print(user_data)
+
+# print(type(user_data))
 
 
 
 # Access keys with brackets
+# print(user_data["name"])
+# print(user_data['user id'])
 
 
 # # Add new key value
@@ -26,8 +32,13 @@ from statistics import mode
 
 
 # Dict constructor
+# new_dictionary = dict()
 
+# print(new_dictionary)
+# print(type(new_dictionary))
 
+# pet_name = dict(name ='fido', age =14)
+# print(pet_name)
 
 
 
@@ -42,11 +53,13 @@ dog = {
 
 # Lets use the keys methods to get the keys from this dictionary
 # Assign keys to a variable
-
+# dog_information = dog.keys()
+# print(dog_information)
 
 
 # Assign values to a variable
-
+# dog.clear()
+# print(dog)
 
 
 # Lets use clear method to remove all elements
@@ -54,35 +67,46 @@ dog = {
 
 
 # Lets use get method to get a key value
-
+# age = dog.get('age')
+# print(age)
 
 
 # # # lets look at one of the parameters to show an error if the key doesnt exist
-
+# print(dog.get('temperament','key does not exist'))
 # Lets create a copy
-
+new_dog = dog.copy()
+# print(new_dog)
+# print(type(new_dog))
 
 # Lets remove a specified key with pop
-
-
+# new_dog.pop('breed')
+# print(new_dog)
 
 # Lets remove a last inserted key-value pair with popitem
-
+# new_dog.popitem('breed')
+# print(new_dog)
 
 # Get a list with each key-value pair with items
 
 
 # we can loop through
-
+for k , v  in new_dog.items():
+    print(k,v)
 
 # Update allows us to update by changing and adding data
-
+dog.update({"temperament":"happy","breed":"chiwahwahua"})
+print(dog)
 
 # Dictionaries vs Lists
 list1 = ['a', 'b', 'c', 'd', 'e']
 dict1 = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 5: 'e'}
- 
+# print(list1[3])
+# print(dict1[3])
 
+# list1[3] = 'z'
+# dict1[3] = 'z'
+# print(list1)
+# print(dict1)
 '''
 Write some code that takes two lists and converts them into one dictionary.
 In:
@@ -94,19 +118,37 @@ Out:
 '''
 list1 = ['one', 'two', 'three']
 list2 = [4, 10, 30]
-
+# new_dict = {}
+# for  n in range(len(list1)):
+#     new_dict.update({list1[n]:list2[n]})
+# print(new_dict)
 
 
 my_keys = ['one', 'two', 'three']
 my_values = [4, 10, 30]
 
 # Using Zip & Dict
+# my_dictionary = dict(zip(my_keys,my_values))
+# print(my_dictionary)
+# # Using dictionary comprehension
+# my_dictionary = {keys: values for (keys , values) in zip ()}
+
+'''Combing Lists: The Zip Function
+The zip() function takes two (or more) lists as inputs and returns an object that contains a list of pairs
+Ex.
+names = [“Jenny”, “Alexus”, “Sam”, “Grace”]
+heights = [61, 70, 67, 64]
+names_and_heights = zip(names, heights)
+# you have to convert the zip object into a list to use it
+converted_list = list(names_and_heights)
+print(converted_list)
+Output:
+[(“Jenny”, 61), (“Alexus”, 70), (“Sam”, 67), (“Grace”, 64)]'''
 
 
-# Using dictionary comprehension
-
-
-
+'''Notice two things:
+1. Our data set has been converted from a zip memory object to an actual list (denoted by [])
+2. Our inner lists don’t use square brackets [] around the values. The is because they have been converted into types (an immutable type of list)'''
 '''
 Exercise
 
@@ -117,13 +159,17 @@ Hint: Use the items() method
 
 words = {"color":"blue", "veggie":"radish", "vehicle": "bike", "mood": "happy", "pet":"cat"}
 
+# print(words.items())
+# for w , c  in words.items():
+#     print(w,c)
 
 
 # As datasets, we can use bracket notation
 
-choices = {"flavors":['strawberry', 'vanilla', 'orange'],
-           "sizes":['large', 'medium', 'small']}
-
+# choices = {"flavors":['strawberry', 'vanilla', 'orange'],
+#            "sizes":['large', 'medium', 'small']}
+# print(choices['flavors'][0])
+# print(choices['sizes'][0])
 
 '''
 Exercise
@@ -136,7 +182,7 @@ Create a dictionary for an automobile including make, model, year, number of doo
 #        "number of doors":4,
 #        "cylinders": 6}
 
-# print(car)
+# print(car.items())
 
 '''
 In statistics, the mode is the value that appears most frequently in a dataset.
@@ -145,15 +191,33 @@ Write some code that uses a dictionary to calculate the mode of a list.
 
 '''
 
+
 my_list_items = [1,2,4,1,3,4,1,1] # our list
+
+output = {} # my final output
+
+# for m in my_list_items:
+#     if m not in output:
+#         output[m]= 1 # add it intially
+#         # print(output)
+#     else:
+#         output[m] += 1
+# print(output)
 
 
 # Statistics module
+use_stat_module = [1,2,4,1,3,4,1,1,10,10,10,10,10]
+result = mode(use_stat_module)
+# print(result)
 
 
 # Looping through and adding 
 incomes = {"apple": 5600.00, "orange": 3500.00, "banana": 5000.00}
+total_income = 0.00
 
+for i in incomes.values():
+    total_income += i 
+    print(total_income)
 
 
 '''
