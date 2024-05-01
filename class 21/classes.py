8a6e059512def61f036589c5fc7b3721a80ff9ff
Farley Digital <farleydigital@farleys-air.mynetworksettings.com>
2024-05-01 18:05:43 -0400

    update

---
+++ class 21/classes.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+import datetime
+
+'''
+Classes
+'''
+# Class Definition and Initializer
+
+
+# Return a string representation of this object
+   
+    
+# Adds this object to another object from the same class, return a new object.
+    
+    
+# Subtracts another object from this object, return a new object.
+    
+
+# Test equality between this object and another, return a boolean.
+   
+     
+    
+# Mutator method
+  
+
+# Accessor method
+   
+
+# Create a point object with attributes x=2, y=3
+
+# Let’s add a __str__ method to our Point2D class so we can print Point2D objects.
+
+
+# Lets add __add__ to add objects of the same class together
+
+
+# Lets try __sub__ to add objects of the same class together
+
+# Let's try __eq__ method to test equality
+'''What is the output of this code if we don't override ==?
+What is the output if we override == to use value equality?
+Is it the same or different? Why?
+
+Without the __eq__ method, we will only be able to test if it is the same object
+'''
+
+
+
+# Let's try __lt__ method to test less than
+
+
+# Setting with mutator methods
+
+
+
+# Getting with accessor methods
+
+
+
+
+
+''' Exercise - Date Class '''
+
+
+
+
+ 
+
+
+
+
+'''
+Exercise - Dog Class
+'''
